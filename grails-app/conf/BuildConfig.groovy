8780b1225e6e856b75573181c04594c16ab5306c
jessica <jessica.aguado@gmail.com>
2015-03-25 11:20:16 +0100

    anadido plugin para sass y anadido compass (copy paste del resultado de ejecutar compass create bideanWebapp)

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -56,12 +56,13 @@ grails.project.dependency.resolution = {
     plugins {
         // plugins for the build system only
         build ":tomcat:7.0.55"
-        build: ":lescss-resources:"
+
 
         // plugins for the compile step
         compile ":scaffolding:2.1.2"
         compile ':cache:1.1.8'
-        compile ":asset-pipeline:1.9.9"
+        compile ":asset-pipeline:2.1.5"
+        compile ":sass-asset-pipeline:2.1.1"
 
         // plugins needed at runtime but not for compilation
         runtime ":hibernate4:4.3.6.1" // or ":hibernate:3.6.10.18"
